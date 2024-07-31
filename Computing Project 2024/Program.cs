4190ce795afaddc8eb69f1f45a5d800b8e69f93d
Laurence <73744180+laurenceg1099@users.noreply.github.com>
2024-07-31 10:55:04 +0100

    Add project files.

---
+++ Computing Project 2024/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Computing_Project_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
