3a12f0bc3d0921b36d97f01efdcded4b08e9a29d
laurenceg1099 <73744180+laurenceg1099@users.noreply.github.com>
2024-12-30 13:03:33 +0000

    slightly better scheduling still not perfect

--- Computing Project 2024/Season.cs
+++ Computing Project 2024/Season.cs
@@ -10,6 +10,7 @@ namespace Computing_Project_2024
     class Season
     {
         private Team _team;
+        private int length = 185;
         private List<Team> _teams;
         public Season(List<Team> teams)
         {
@@ -29,47 +30,47 @@ namespace Computing_Project_2024
 
         private void CreateSchedule()
         {
-            int Divmax = 76/2;
-            int Leaguemax = 66/2;
+            int Divmax = 76 / 2;
+            int Leaguemax = 66 / 2;
             int Intermax = 20 / 2;
 
             var teamDict = new Dictionary<string, string> { { "Orioles", "AL East" }, { "Red-Sox", "AL East" }, { "Yankees", "AL East" }, { "Rays", "AL East" }, { "Blue-Jays", "AL East" }, { "White-Sox", "AL Central" }, { "Guardians", "AL Central" }, { "Tigers", "AL Central" }, { "Royals", "AL Central" }, { "Twins", "AL Central" }, { "Astros", "AL West" }, { "Angels", "AL West" }, { "Athletics", "AL West" }, { "Mariners", "AL West" }, { "Rangers", "AL West" }, { "Braves", "NL East" }, { "Marlins", "NL East" }, { "Mets", "NL East" }, { "Phillies", "NL East" }, { "Nationals", "NL East" }, { "Cubs", "NL Central" }, { "Reds", "NL Central" }, { "Brewers", "NL Central" }, { "Pirates", "NL Central" }, { "Cardinals", "NL Central" }, { "Diamondbacks", "NL West" }, { "Rockies", "NL West" }, { "Dodgers", "NL West" }, { "Padres", "NL West" }, { "Giants", "NL West" } };
             var random = new Random();
             var schedule = new List<Series> { };
-            
+
             foreach (var hometeam in _teams)
             {
                 int Divisional = 0;
                 int League = 0;
                 int InterLeague = 0;
-    
 
-                while (Divisional < Divmax || League < Leaguemax || InterLeague<Intermax)
+
+                while (Divisional < Divmax || League < Leaguemax || InterLeague < Intermax)
                 {
                     int randomIndex = random.Next(_teams.Count);
                     var awayteam = _teams[randomIndex];
 
                     teamDict.TryGetValue(hometeam.Name, out var homeDiv); teamDict.TryGetValue(awayteam.Name, out var awayDiv);
-                    
+
                     if (hometeam == awayteam) continue;
 
                     else if (homeDiv == awayDiv && Divisional < Divmax)
                     {
                         int games = random.Next(2) == 0 ? 3 : 4;
-                        games = Math.Min(games, Divmax-Divisional);
+                        games = Math.Min(games, Divmax - Divisional);
                         Divisional += games;
                         schedule.Add(new Series(hometeam, awayteam, games));
                     }
 
-                    else if (homeDiv.Substring(0,2) == awayDiv.Substring(0, 2) && League < Leaguemax)
+                    else if (homeDiv.Substring(0, 2) == awayDiv.Substring(0, 2) && League < Leaguemax)
                     {
                         int games = random.Next(2) == 0 ? 3 : 4;
                         games = Math.Min(games, Leaguemax - League);
                         League += games;
                         schedule.Add(new Series(hometeam, awayteam, games));
                     }
-                    
-                    else if (homeDiv.Substring(0,2) != awayDiv.Substring(0, 2) && InterLeague < Intermax)
+
+                    else if (homeDiv.Substring(0, 2) != awayDiv.Substring(0, 2) && InterLeague < Intermax)
                     {
                         int games = random.Next(2) == 0 ? 2 : 3; ;
                         games = Math.Min(games, Intermax - InterLeague);
@@ -78,24 +79,48 @@ namespace Computing_Project_2024
                     }
 
                 }
-                
-           
+
+
 
             }
 
-            Console.WriteLine(schedule.Sum(x => x._Games));
-
-            for (int i = 0; i < schedule.Count; i++)
-            {
-                int r = random.Next(schedule.Count);
-                (schedule[i], schedule[r]) = (schedule[r], schedule[i]);
-            }
-
+            schedule = ShuffleGames(schedule);
             writeGames(schedule);
 
-            Console.WriteLine(schedule.Sum(x => x._Games));
 
-      
+
+
+        }
+
+        private List<Series> ShuffleGames(List<Series> schedule)
+        {
+            int lookback = 15;
+            List<Series> result = new List<Series>();
+            var random = new Random();
+            while (schedule.Count > 0)
+            {
+                var prevteams = result.TakeLast(lookback).SelectMany(x => new[] { x.Hometeam, x.Awayteam }).Cast<Team>().ToList();
+                var available = schedule.Select((series,idx) => new { series, idx }).Where(x => !prevteams.Contains(x.series.Hometeam) && !prevteams.Contains(x.series.Awayteam)).ToList();
+                
+                if (available.Count != 0)
+                {
+                    var r = random.Next(available.Count);
+                    var nextseries = available[r];
+                    var index = nextseries.idx;
+                    result.Add(nextseries.series);
+                    schedule.RemoveAt(index);
+                    lookback = 15;
+                }
+                else
+                {
+                    lookback--;
+                }
+                
+       
+
+            }
+
+            return result;
 
         }
 
